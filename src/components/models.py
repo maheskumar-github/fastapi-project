c9886eed06dad02ad7c5c2a84d3b2a22d7e9dbb7
Maheskumar Palanimuthu <maheskumar.palani@gmail.com>
2024-10-13 08:52:03 +0530

    sql alchemy

--- src/components/models.py
+++ src/components/models.py
@@ -4,3 +4,25 @@ from src.exception import CustomException
 from src.logger import logging
 
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from database import Base
+
+class Question(Base):
+    __tablename__ = 'questions'
+
+    id = Column(Integer, primary_key=True, index=True)
+    question_text = Column(String, index=True)
+
+    logging.info("Questions table is create")
+
+class Choices(Base):
+    __tablename__ = 'choices'
+
+    id = Column(Integer, primary_key=True, index=True)
+    choices_text = Column(String, index=True)
+    is_correct = Column(Boolean, default=False)
+    question_id = Column(Integer, ForeignKey("questions.id"))
+
+    logging.info("Choices table is create")
+
+
+
